9ee611ed69dfcab69e5e6cdaffabdf94af154cc0
fuatguzel <fuatguzel754@gmail.com>
2021-09-17 16:08:25 +0300

    Add react-intl lib

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
 
+import { useState } from 'react';
+import {IntlProvider, FormattedMessage} from 'react-intl'
+
+const messages = {
+  "en-US": {
+    title: "Hello World",
+    description: "Life is beautiful."
+  },
+  "tr-TR": {
+    title: "Merhaba Dünya",
+    description: "Hayat güzel."
+  }
+}
+
 function App() {
+  const [lang, setLang] = useState("en-US")
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <IntlProvider messages={messages[lang]}>
+      <p>
+        <FormattedMessage
+          id="title"
+        />
+      </p>
+      <br />
+      <p>
+        <FormattedMessage
+          id="description"
+        />
+      </p>
+      <br/><br/>
+      <button onClick={() => setLang('tr-TR')}>TR</button>
+      <button onClick={() => setLang('en-US')}>EN</button>
+      </IntlProvider>
     </div>
   );
 }
