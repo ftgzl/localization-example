035f90d3129903c8afa128f49a1c4f633eeeae1f
fuatguzel <fuatguzel754@gmail.com>
2021-09-17 16:33:18 +0300

    Add parametres

--- src/App.js
+++ src/App.js
@@ -3,14 +3,18 @@ import './App.css';
 import { useState, useEffect } from 'react';
 import {IntlProvider, FormattedMessage} from 'react-intl'
 
+const bicycleprice = 100
+const dolarToTry = 8
 const messages = {
   "en-US": {
-    title: "Hello World",
-    description: "Life is beautiful."
+    product: "Bicycle",
+    description: "Has 2 wheel.",
+    price: "{price}USD"
   },
   "tr-TR": {
-    title: "Merhaba Dünya",
-    description: "Hayat güzel."
+    product: "Bisiklet",
+    description: "2 tekerleği var.",
+    price: "{price}TRY"
   }
 }
 
@@ -28,16 +32,22 @@ function App() {
       <IntlProvider locale={locale} messages={messages[locale]}>
       <p>
         <FormattedMessage
-          id="title"
+          id="product"
         />
       </p>
-      <br />
       <p>
         <FormattedMessage
           id="description"
         />
       </p>
-      <br/><br/>
+      <br/>
+      <p>
+        <FormattedMessage values={{price : 
+          locale==="en-US" ? bicycleprice : bicycleprice * dolarToTry}}
+          id="price"
+        />
+      </p>
+      <br/>
       <button onClick={() => setLocale('tr-TR')}>TR</button>
       <button onClick={() => setLocale('en-US')}>EN</button>
       </IntlProvider>
